bb0f0d6af1a15feba1d918484cb21788127820f2
RebecaCamilo <rebecacamilo3@gmail.com>
2024-10-03 20:41:33 -0300

    feat: add test to login modal

--- client/cypress/e2e/hero-home-page.cy.ts
+++ client/cypress/e2e/hero-home-page.cy.ts
@@ -3,7 +3,7 @@
 describe("hero-home-page", () => {
 
     describe('User not logged', () => {
-        it.skip('reload the page by clicking in title and still not logged', () => {
+        it('reload the page by clicking in title and still not logged', () => {
             cy.visit('http://localhost:3000/heroes');
             cy.contains('button', 'Login').should('be.visible');
 
@@ -27,6 +27,19 @@ describe("hero-home-page", () => {
             cy.get('.open.modal').should('be.visible');
             cy.contains('h5', 'You must log in to hire this hero.').should('be.visible');
             cy.contains('button', 'Ok').should('be.visible');
+        });
+
+        it.only('user not logged can see the login button', () => {
+            cy.visit('http://localhost:3000/heroes');
+            cy.contains('button', 'Login').click();
+
+            cy.contains('h5', 'Login').should('be.visible');
+
+            cy.get('[data-cy="email"]').should('exist');
+            cy.get('[data-cy="email"]').parents('label').should('contain.text', 'Email');
+
+            cy.get('[data-cy="password"]').should('exist');
+            cy.get('[data-cy="password"]').parents('label').should('contain.text', 'Password');
         });
         
       });
